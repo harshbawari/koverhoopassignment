e70d0d31aa0e355f4399e7b378afef5caace154b
harshbawari <harshbawari@gmail.com>
2020-07-31 23:20:32 +0530

    Responsive UI Completed

--- src/components/Content.js
+++ src/components/Content.js
@@ -47,45 +47,44 @@ class Content extends Component{
         const renderResults = this.state.movieList.map((movie, i) => {
             return(
                 
-                <div key={i} className="col-3 movieMedia">
+                <div key={i} className="col-12 col-md-6 col-lg-3 movieMedia">
                     <div className="media d-inline-block">
-                        <img className="mt-2 img-thumbnail align-self-center" src={movie.Poster} />
+                        <img className="mt-2 img-thumbnail align-self-center mainImageThumbnail" src={movie.Poster} />
                         <div className="media-body">
                             <h4>{movie.Title}</h4>
                         </div>
                         <input className="btn btn-primary" value="Movie Details" onClick={(e) => {this.setState({ modalIsOpen: true}); this.fetchMovieDetails(movie.Title)}} id={i} />
+                        
+                        {/*Modal to display Movie Details */}
                         {this.state.data != null ? <Modal className="modal-lg" size="md" isOpen={this.state.modalIsOpen}>
                             <ModalHeader className="flex-row-reverse">
                                 <div className="ml-auto"><button className="btn btn-danger" onClick={(e) => this.setState({ modalIsOpen: false})}>Close</button></div>
                             </ModalHeader>
-                            <ModalBody className="col col-sm col-sm-first align-items-center justify-content-center">
+                            <ModalBody className="align-items-center justify-content-center">
                                 <div className="card">
-                                    <img className="ml-2 order-md-first p-4 img-thumbnail" src={this.state.data.Poster} />
-                                    <div className="card-body">
-                                        <h3 className="justify-self-center">{this.state.data.Title}</h3>
-                                        <div className="movieDetails p-3 d-block">
-                                            <dl className="row">
-                                                <dt className="col-6">Genre: </dt><dd className="col-6">{this.state.data.Genre}</dd>
-                                                <dt className="col-6">Released: </dt><dd className="col-6">{this.state.data.Released}</dd>
-                                                <dt className="col-6">Rated: </dt><dd className="col-6">{this.state.data.Rated}</dd>
-                                                <dt className="col-6">IMDB Rating: </dt><dd className="col-6">{this.state.data.imdbRating}</dd>
-                                                <dt className="col-6">Director: </dt><dd className="col-6">{this.state.data.Director}</dd>
-                                                <dt className="col-6">Writer: </dt><dd className="col-6">{this.state.data.Writer}</dd>
-                                                <dt className="col-6">Actors: </dt><dd className="col-6">{this.state.data.Actors}</dd>
-                                            </dl>
+                                    <div className="card-body d-block">
+                                        <img className="ml-2 col-12 col-lg-6 order-first p-4 img-thumbnail thumbImage" src={this.state.data.Poster} />
+                                        <div className="col-12 col-lg-6">
+                                            <h3 className="justify-self-center">{this.state.data.Title}</h3>
+                                            <div className="movieDetails p-3 d-block">
+                                                <dl className="row">
+                                                    <dt className="col-6">Genre: </dt><dd className="col-6">{this.state.data.Genre}</dd>
+                                                    <dt className="col-6">Released: </dt><dd className="col-6">{this.state.data.Released}</dd>
+                                                    <dt className="col-6">Rated: </dt><dd className="col-6">{this.state.data.Rated}</dd>
+                                                    <dt className="col-6">IMDB Rating: </dt><dd className="col-6">{this.state.data.imdbRating}</dd>
+                                                    <dt className="col-6">Director: </dt><dd className="col-6">{this.state.data.Director}</dd>
+                                                    <dt className="col-6">Writer: </dt><dd className="col-6">{this.state.data.Writer}</dd>
+                                                    <dt className="col-6">Actors: </dt><dd className="col-6">{this.state.data.Actors}</dd>
+                                                </dl>
+                                            </div>
                                         </div>
                                     </div>
                                     <div className="card-footer">
-                                        <h3 className="col-12">
-                                            Plot
-                                        </h3>
+                                        <h3>Plot</h3><br />
                                         <p>{this.state.data.Plot}</p>
                                     </div>
                                 </div>
                             </ModalBody>
-                            <ModalFooter>
-
-                            </ModalFooter>
                         </Modal> : <div></div>}
                     </div>
                 </div>
@@ -93,7 +92,7 @@ class Content extends Component{
         });
 
         return(
-            <div className="row d-flex">
+            <div className="row d-inline-flex">
                 {this.state.loading? <div><span className="badge badge-secondary">Loading...</span></div> : renderResults}
             </div>
         );
--- src/components/Head.js
+++ src/components/Head.js
@@ -29,18 +29,18 @@ class Head extends Component {
         return(
             <div className="container-fluid">
                 <div className="row d-flex headerSection justify-content-center align-items-center">
-                    <div className="col-3">
+                    <div className="col-12 col-lg-3">
                         <h1>MovieSearcher</h1>
                     </div>
-                    <div className="col-6 d-inline-block">
+                    <div className="col-12 col-lg-6 d-inline-block">
                         <input type="text" placeholder="Search Movie Here" name="searchMovie" id="searchMovie" onChange={this.handleChange}></input>
                     </div>
-                    <div className="col-1">
+                    <div className="col-6 col-lg-1">
                         <select value="category" id="category">
                             <option>Movie</option>
                         </select>
                     </div>
-                    <div className="col-2">
+                    <div className="col-6 col-lg-2">
                         <input className="btn btn-primary" type="submit" id="submitButton" value="Submit" onClick={this.handleSubmit}></input>
                     </div>
                 </div>
